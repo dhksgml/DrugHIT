30227013a7fdad6a51bcc1665bcb2aa455db5da7
dhksgml <69703890+dhksgml@users.noreply.github.com>
2022-11-13 23:39:31 +0900

    반격과 데미지 처리
    데미지에 따라서 체력바 상태 변경됨
    +반격에 대한 데미지 처리는 아직 구현 안함

--- Assets/Scripts/HitManager.cs
+++ Assets/Scripts/HitManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HitManager : MonoBehaviour
 {
@@ -8,6 +9,11 @@ public class HitManager : MonoBehaviour
     private Player1 p1;
     [SerializeField]
     private Player2 p2;
+
+    [SerializeField]
+    private Slider p1Hpbar;
+    [SerializeField]
+    private Slider p2Hpbar;
 
     public bool isHeadWeakHit1 = false;
     public bool isHeadStrongHit1 = false;
@@ -24,37 +30,40 @@ public class HitManager : MonoBehaviour
     private float bodyHitDelay = 0.3f;
     private float legHitDelay = 0.3f;
 
+    private bool isDieP1 = false;
+    private bool isDieP2 = false;
+
     private void Start()
     {
-       
+
     }
 
     private void Update()
     {
         if (isHeadWeakHit2)
         {
-            Debug.Log("2p 약하게 머리 맞음");
+            //Debug.Log("2p 약하게 머리 맞음");
             isHeadWeakHit2 = false;
             //StopAllCoroutines();
             StartCoroutine(p2.hwDamaged(headWHitDelay));
         }
         if (isHeadStrongHit2)
         {
-            Debug.Log("2p 세게 머리 맞음");
+            //Debug.Log("2p 세게 머리 맞음");
             isHeadStrongHit2 = false;
             //StopAllCoroutines();
             StartCoroutine(p2.hsDamaged(headSHitDelay));
         }
         if (isBodyHit2)
         {
-            Debug.Log("2p 몸통 맞음");
+            //Debug.Log("2p 몸통 맞음");
             isBodyHit2 = false;
             //StopAllCoroutines();
             StartCoroutine(p2.bDamaged(bodyHitDelay));
         }
         if (isLegHit2)
         {
-            Debug.Log("2p 다리 맞음");
+            //Debug.Log("2p 다리 맞음");
             isLegHit2 = false;
             //StopAllCoroutines();
             StartCoroutine(p2.lDamaged(legHitDelay));
@@ -63,31 +72,46 @@ public class HitManager : MonoBehaviour
 
         if (isHeadWeakHit1)
         {
-            Debug.Log("1p 약하게 머리 맞음");
+            //Debug.Log("1p 약하게 머리 맞음");
             isHeadWeakHit1 = false;
             //StopAllCoroutines();
             StartCoroutine(p1.hwDamaged(headWHitDelay));
         }
         if (isHeadStrongHit1)
         {
-            Debug.Log("1p 세게 머리 맞음");
+            //Debug.Log("1p 세게 머리 맞음");
             isHeadStrongHit1 = false;
             //StopAllCoroutines();
             StartCoroutine(p1.hsDamaged(headSHitDelay));
         }
         if (isBodyHit1)
         {
-            Debug.Log("1p 몸통 맞음");
+            //Debug.Log("1p 몸통 맞음");
             isBodyHit1 = false;
             //StopAllCoroutines();
             StartCoroutine(p1.bDamaged(bodyHitDelay));
         }
         if (isLegHit1)
         {
-            Debug.Log("1p 다리 맞음");
+            //Debug.Log("1p 다리 맞음");
             isLegHit1 = false;
             //StopAllCoroutines();
             StartCoroutine(p1.lDamaged(legHitDelay));
         }
+
+        p1Hpbar.value = p1.curHP;
+        p2Hpbar.value = p2.curHP;
+    }
+
+    public void DieP1()
+    {
+        isDieP1 = true;
+        Debug.Log("1p 죽음");
+    }
+
+    public void DieP2()
+    {
+        isDieP2 = true;
+        Debug.Log("2p 죽음");
     }
 }
